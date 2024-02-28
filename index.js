7c276d228edd4dc83efe87aa7b38ed6bde1ff80e
rifai <alliiyassir@gmail.com>
2024-02-28 08:58:53 +0100

    version 1.1

--- index.js
+++ index.js
@@ -3,13 +3,21 @@ const apiUrl = "http://quotable.io/random";
 const quoteEl = document.getElementById("span-quote");
 const authorEl = document.getElementById("author");
 
-
 async function get_quote() {
-  const response = await fetch(apiUrl);
-  const data = await response.json();
-  quoteEl.innerText = data.content;
-  authorEl.innerText = data.author;
-
+  try {
+    btnEl.innerText= 'loading...';
+    btnEl.disabled = true;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    quoteEl.innerText = data.content;
+    authorEl.innerText = data.author;
+    btnEl.innerText= 'Get A Quote';
+    btnEl.disabled = false;
+  } catch (error) {
+    console.log(error);
+    quoteEl.innerText = "Ops something went wrong,try again later.";
+    authorEl.innerText = '';
+  }
 }
 
 btnEl.addEventListener("click", get_quote);
